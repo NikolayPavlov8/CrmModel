eea85e92d6ac54fc31d8f8c9fe6e44d65c215afa
Nikolay Pavlov8 <nikolaspavlov8@gmail.com>
2019-07-17 13:45:09 +0300

    code1

---
+++ CrmModelShop/CrmBL/Model/Check.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model
+{
+    class Check
+    {
+        public int CheckId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        public int SellerId { get; set; }
+
+        public virtual Seller Seller { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public override string ToString()
+        {
+            return $"№{CheckId} от {Created.ToString("dd.MM.yy hh:mm:ss")}";
+        }
+    }
+}
